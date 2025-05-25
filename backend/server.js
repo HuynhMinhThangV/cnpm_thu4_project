32ca7befabf1b329297c0926bcabf6be391dcee7
ViMinhThang <huynhminhthang246@gmail.com>
2025-05-25 21:08:17 +0700

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(
   cors({
-    origin: "https://cnpmthu4project.vercel.app/",
+    origin: "https://cnpmthu4project.vercel.app",
   })
 );
 app.use(express.json());
