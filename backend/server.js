954a92bb41730b28316a0e6f46a799aff2adec56
ViMinhThang <huynhminhthang246@gmail.com>
2025-05-25 21:05:47 +0700

    adding cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://cnpmthu4project.vercel.app/",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
